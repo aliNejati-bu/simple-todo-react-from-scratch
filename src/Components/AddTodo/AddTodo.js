dcd2dd042a535b8d4e19530e1354c4524cc4d041
aliNejati-bu <electrocellco@gmail.com>
2023-06-17 05:09:53 +0330

    add TODO Input.

--- src/Components/AddTodo/AddTodo.js
+++ src/Components/AddTodo/AddTodo.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
+
+import "./AddTodo.css";
 
 const AddTodo = props => {
     return (
         <div className='add-card'>
             <div className='add-card_input'>
-                <input type="text"/>
+                <input type="text" placeholder="Enter New Item Here"/>
             </div>
             <div className='add-card__btn'>
                 <button>add new</button>
